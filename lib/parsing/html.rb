2a380cc69471609d5d18478c5ec06ec044389e3b
Siddhant Kumar <siddhantkumar.xyz@gmail.com>
2021-12-25 10:43:14 +0530

    Parse HTML

--- lib/parsing/html.rb
+++ lib/parsing/html.rb
@@ -18,8 +18,18 @@ module Parsing
     def get
       # TODO: Parse HTML, create person, add to people
       # HTML loop {
+      Nokogiri::HTML(open(HTML_DATA)).css('tr').each do |row|
+        
       #   person = Person.new()
+        person = Person.new( 
+          first_name: row.css('td')[0].text,
+          last_name: row.css('td')[1].text,
+          title: row.css('td')[2].text,
+          Specialities: row.css('td')[3].text
+        )
       #   @people.add(person)
+        @people.add(person)
+      end
       # }
     end
   end
