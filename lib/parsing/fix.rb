3b0c278686b163cf7d491047e3b92d40b011c7da
Siddhant Kumar <siddhantkumar.xyz@gmail.com>
2022-01-03 23:43:18 +0530

    modify fix.rb for CLI

--- lib/parsing/fix.rb
+++ lib/parsing/fix.rb
@@ -28,7 +28,7 @@ module Parsing
     end
 
     # Parse country from the location value
-    # @param name [String] Location from the data source
+    # @param location [String] Location from the data source
     # @return [String] The country value
     def self.location(location)
       md = /\A(?<country>[^-]+)-.*\z/.match(location)
@@ -41,6 +41,16 @@ module Parsing
     def self.better_name(name)
       BETTER_NAME.each { |k, v| name = v if name == k }
       name
+    end
+
+    # Creates name key from the first and last name
+    # @param hash [Hash] Hash from person object
+    # @return [Hash] Hash with first and last name removed
+    def self.full_name(hash)
+      hash[:name] = "#{hash[:first_name]} #{hash[:last_name]}"
+      hash.delete(:first_name)
+      hash.delete(:last_name)
+      hash
     end
   end
 end
