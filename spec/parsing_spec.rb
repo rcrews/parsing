225a5464173ef3419aa452a204b1c5ca1ccd837e
Siddhant Kumar <siddhantkumar.xyz@gmail.com>
2022-01-10 17:00:01 +0000

    rubocop

--- spec/parsing_spec.rb
+++ spec/parsing_spec.rb
@@ -6,38 +6,38 @@ RSpec.describe Parsing do # rubocop:disable Metrics/BlockLength
   end
 
   it 'has access to CSV data' do
-    expect(URI Parsing::CSV_DATA).not_to be nil
+    expect(URI(Parsing::CSV_DATA)).not_to be nil
   end
 
   it 'has access to HTML data' do
-    expect(URI Parsing::HTML_DATA).not_to be nil
+    expect(URI(Parsing::HTML_DATA)).not_to be nil
   end
 
   it 'has access to JSON data' do
-    expect(URI Parsing::JSON_DATA).not_to be nil
+    expect(URI(Parsing::JSON_DATA)).not_to be nil
   end
 
   it 'has access to XML data' do
-    expect(URI Parsing::XML_DATA).not_to be nil
+    expect(URI(Parsing::XML_DATA)).not_to be nil
   end
 
   it 'has access to YAML data' do
-    expect(URI Parsing::YAML_DATA).not_to be nil
+    expect(URI(Parsing::YAML_DATA)).not_to be nil
   end
 
   it 'has an ERB template for HTML output' do
-    expect(File.exist? Parsing::HTML_TEMPLATE).to be true
+    expect(File.exist?(Parsing::HTML_TEMPLATE)).to be true
   end
 
   it 'has an ERB template for text output' do
-    expect(File.exist? Parsing::TEXT_TEMPLATE).to be true
+    expect(File.exist?(Parsing::TEXT_TEMPLATE)).to be true
   end
 
   it 'has an ERB template for displaying the version from the CLI' do
-    expect(File.exist? Parsing::VERSION_TEMPLATE).to be true
+    expect(File.exist?(Parsing::VERSION_TEMPLATE)).to be true
   end
 
   it 'has an ERB template for displaying usage from the CLI' do
-    expect(File.exist? Parsing::HELP_TEMPLATE).to be true
+    expect(File.exist?(Parsing::HELP_TEMPLATE)).to be true
   end
 end
